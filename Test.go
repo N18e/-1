dd3ffb40e91c7c3e57866affabbd7c444ba0df2a
N18e <neptune.energy18@gmail.com>
2025-01-13 13:45:06 +0200

    Обновлен test.go: теперь warrior 33 года

--- Test.go
+++ Test.go
@@ -5,11 +5,8 @@ import (
 )
 
 func main() {
-	s := "gopher"
-	var name string = "Sergey"
-	fmt.Println("Hello and welcome, %s!", s)
-	fmt.Println("Hi my name is", name)
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
-	}
+	s := "varrior"
+	var old int = 33
+	fmt.Println("This is", s, "He is",
+		old, "years old")
 }
